f16660539f4a631c83ed59865a884d20667feb07
Ajay Gokhale <ajay.g@callme.co.in>
2020-12-22 12:22:10 +0530

    Pagination added without functionality. ;)

---
+++ src/common/pagination.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import _ from "lodash";
+const Pagination = (props) => {
+  const { itemCount, pageSize, currentPage, onPageChange } = props;
+  const pageCount = Math.ceil(itemCount / pageSize);
+
+  if (pageCount === 1) return null;
+
+  const pages = _.range(1, pageCount + 1);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a onClick={() => onPageChange(page)} className="page-link">
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
--- src/components/employees.jsx
+++ src/components/employees.jsx
@@ -1,49 +1,69 @@
-import React, { Component } from 'react';
-import {getEmployees} from '../serives/fakeEmployeesList';
-
+import React, { Component } from "react";
+import { getEmployees } from "../serives/fakeEmployeesList";
+import Pagination from "../common/pagination";
 
 class Employees extends Component {
-    state = { 
-        employees: getEmployees(),
-     }
-    render() { 
+  state = {
+    employees: getEmployees(),
+    currentPage: 1,
+    pageSize: 4,
+  };
 
-        if(this.state.employees.length === 0 ) return <p>There are no employees in your company</p>
-        return ( 
-            <React.Fragment>
-                <p>Total Number of Employees : {this.state.employees.length}</p>
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Designation</th>
-                            <th>Address</th>
-                            <th>Mobile Number</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.employees.map(employee => 
-                        <tr key={employee._id}>
-                            <td>{employee.name}</td>
-                            <td>{employee.designation.name}</td>
-                            <td>{employee.address}</td>
-                            <td>{employee.mobileNumber}</td>
-                            <td><button onClick={() => this.onDeteleEmployee(employee)} className="btn btn-danger">Delete</button></td>
-                        </tr>)}
-                        
-                    </tbody>
-                </table>
-            </React.Fragment>
-         );
-    }
+  onDeteleEmployee = (employee) => {
+    const employees = this.state.employees.filter(
+      (e) => e._id !== employee._id
+    );
+    this.setState({ employees });
+  };
 
-    onDeteleEmployee = (employee) =>
-    {
-            const employees = this.state.employees.filter(e => e._id !== employee._id);
-            this.setState({employees})
-    }
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
 
+  render() {
+    if (this.state.employees.length === 0)
+      return <p>There are no employees in your company</p>;
+    return (
+      <React.Fragment>
+        <p>Total Number of Employees : {this.state.employees.length}</p>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Designation</th>
+              <th>Address</th>
+              <th>Mobile Number</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map((employee) => (
+              <tr key={employee._id}>
+                <td>{employee.name}</td>
+                <td>{employee.designation.name}</td>
+                <td>{employee.address}</td>
+                <td>{employee.mobileNumber}</td>
+                <td>
+                  <button
+                    onClick={() => this.onDeteleEmployee(employee)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemCount={this.state.employees.length}
+          pageSize={this.state.pageSize}
+          currentPage={this.state.currentPage}
+          onPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default Employees;
